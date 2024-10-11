29208052d08e7bd9761574705f1374035f6a18bf
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-10-11 16:26:10 -0500

    feat(front): pagina de inicio creada y rutas configuradas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { InvestmentFundsComponent } from './views/investment-funds/investment-funds.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'fondos-inversion',
+    component: InvestmentFundsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ frontend/src/app/views/investment-funds/investment-funds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-funds',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-funds.component.html',
+  styleUrl: './investment-funds.component.css'
+})
+export class InvestmentFundsComponent {
+
+}
